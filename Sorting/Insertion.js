f67cd88a366615eb4b743eee220027ebf3bfcf06
niemet0502 <vincentmarius8@gmail.com>
2022-04-17 13:02:20 +0100

    add insertion sort algo

---
+++ Sorting/Insertion.js
@@ -0,0 +1,23 @@
+const arr = [8, 5, 7, 3, 2];
+
+function insertionSort(arr, n) {
+  let i, key, j;
+  for (i = 1; i < n; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+// console.log(arr);
+// console.log(insertionSort(arr, 5));
