181d4b97a3e03210620d61312215b1ad7d9f9b06
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-06-06 11:40:13 +0000

    added a second approach for the valid palindrom II leetcode question

--- LeetCode/680ValidPalindromeII.js
+++ LeetCode/680ValidPalindromeII.js
@@ -36,3 +36,24 @@
     Time Complexity: O(n^2)
     Space Complexity: O(1)
 */
+
+
+/**
+ * Second approach
+ */
+
+
+ var validPalindrome = function(s) {
+    if(checkPalindrome(s)) return true;
+    for(let i=0;i<s.length/2;i++){
+        if(s.charAt(i)!=s.charAt(s.length-1-i))
+            return checkPalindrome(s.substring(0,i)+s.substring(i+1)) || 
+                checkPalindrome(s.substring(0,s.length-1-i)+s.substring(s.length-i));
+    }
+};
+var checkPalindrome = function(s){
+    for(let i=0;i<s.length/2;i++){
+        if(s.charAt(i)!=s.charAt(s.length-1-i)) return false;
+    }
+    return true;
+}
