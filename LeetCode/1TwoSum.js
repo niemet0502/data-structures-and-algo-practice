1305223bb3be04f29673f2f3850ebdbe5a2ec57a
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-06-13 22:06:12 +0000

    added 1. Two Sum leetcode question

---
+++ LeetCode/1TwoSum.js
@@ -0,0 +1,27 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+ 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+/**
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
