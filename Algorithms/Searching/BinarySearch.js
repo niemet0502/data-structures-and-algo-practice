6dc36bc97ced30e879efb7379b2f11683971fa45
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-21 13:38:17 +0000

    added binary search recursive

--- Algorithms/Searching/BinarySearch.js
+++ Algorithms/Searching/BinarySearch.js
@@ -2,31 +2,41 @@
  * Iterative solution
  */
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var search = function (nums, target) {
-  let start = 0,
-    end = nums.length - 1;
+function binarySearchIterative(arr, x, l, h) {
+  while (l <= h) {
+    mid = Math.floor((l + h) / 2);
 
-  // Iterate while start not meets end
-  while (start <= end) {
-    // Find the mid index
-    let mid = Math.floor((start + end) / 2);
-
-    // If element is present at mid, return True
-    if (nums[mid] === target) return mid;
-    // Else look in left or right half accordingly
-    else if (nums[mid] < target) start = mid + 1;
-    else end = mid - 1;
+    if (arr[mid] === x) {
+      return mid;
+    } else {
+      if (arr[mid] > x) {
+        h = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
   }
 
   return -1;
-};
-
+}
 /**
  * Recursive Solution
  *
  */
+function binarySearchRecursive(arr, x, l, h) {
+  if (l <= h) {
+    mid = Math.floor((l + h) / 2);
+
+    if (arr[mid] === x) {
+      return mid;
+    } else {
+      if (arr[mid] > x) {
+        return binarySearchRecursive(arr, x, l, mid - 1);
+      } else {
+        return binarySearchRecursive(arr, x, mid + 1, h);
+      }
+    }
+  }
+
+  return -1;
+}
