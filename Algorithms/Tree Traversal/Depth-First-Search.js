53a7f081ba5fbf78b36b5f15407a887783fe6738
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-31 20:46:29 +0000

    add dfs tree inorder methods, recursive and iterative

--- Algorithms/Tree Traversal/Depth-First-Search.js
+++ Algorithms/Tree Traversal/Depth-First-Search.js
@@ -1,8 +1,8 @@
 function dfsPreOrderRecursive(node, arr = []) {
   if (node) {
     arr.push(node.val);
-    if (node.left) return dfsPreOrderRecursive(node.left, arr);
-    if (node.right) return dfsPreOrderRecursive(node.right, arr);
+    if (node.left) dfsPreOrderRecursive(node.left, arr);
+    if (node.right) dfsPreOrderRecursive(node.right, arr);
   }
 
   return arr;
@@ -19,6 +19,34 @@ function dfsPreOrderIterative(root) {
     result.push(curr.val);
     if (curr.right) stack.push(curr.right);
     if (curr.left) stack.push(curr.left);
+  }
+
+  return result;
+}
+
+function dfsInOrderRecursive(node, arr = []) {
+  if (node) {
+    if (node.left) dfsInOrderRecursive(node.left, arr);
+    arr.push(node.val);
+    if (node.right) dfsInOrderRecursive(node.right, arr);
+  }
+
+  return arr;
+}
+
+function dfsInOrderIterative(root) {
+  const stack = [];
+  const result = [];
+  let curr = root;
+
+  while (stack.length || curr) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    result.push(curr.val);
+    curr = curr.right;
   }
 
   return result;
