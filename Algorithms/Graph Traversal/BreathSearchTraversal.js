9e8e900a89752b78075d1922d68168e01543c1ca
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-09-20 06:38:09 +0000

    add breadth first search for graph

---
+++ Algorithms/Graph Traversal/BreathSearchTraversal.js
@@ -0,0 +1,25 @@
+// function bfs(adjacencyList) {
+//   const [firstKey] = adjacencyList.keys();
+
+//   const visited = new Set();
+//   visited.add(firstKey);
+
+//   const queue = [firstKey];
+
+//   const result = [];
+
+//   while (queue.length) {
+//     current = queue.shift();
+//     adjacencyNodes = adjacencyList.get(current);
+
+//     for (const node of adjacencyNodes) {
+//       if (!visited.has(node)) {
+//         result.push(node);
+//         visited.add(node);
+//         queue.push(node);
+//       }
+//     }
+//   }
+
+//   return result;
+// }
