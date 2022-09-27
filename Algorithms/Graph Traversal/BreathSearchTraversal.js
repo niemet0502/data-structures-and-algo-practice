57b30d20d691069b25e15d70806709e8acf8e1f7
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-09-27 08:47:49 +0000

    create a grap from a given array

--- Algorithms/Graph Traversal/BreathSearchTraversal.js
+++ Algorithms/Graph Traversal/BreathSearchTraversal.js
@@ -1,25 +1,25 @@
-// function bfs(adjacencyList) {
-//   const [firstKey] = adjacencyList.keys();
+function bfs(adjacencyList) {
+  const [firstKey] = adjacencyList.keys();
 
-//   const visited = new Set();
-//   visited.add(firstKey);
+  const visited = new Set();
+  visited.add(firstKey);
 
-//   const queue = [firstKey];
+  const queue = [firstKey];
 
-//   const result = [];
+  const result = [];
 
-//   while (queue.length) {
-//     current = queue.shift();
-//     adjacencyNodes = adjacencyList.get(current);
+  while (queue.length) {
+    current = queue.shift();
+    adjacencyNodes = adjacencyList.get(current);
 
-//     for (const node of adjacencyNodes) {
-//       if (!visited.has(node)) {
-//         result.push(node);
-//         visited.add(node);
-//         queue.push(node);
-//       }
-//     }
-//   }
+    for (const node of adjacencyNodes) {
+      if (!visited.has(node)) {
+        result.push(node);
+        visited.add(node);
+        queue.push(node);
+      }
+    }
+  }
 
-//   return result;
-// }
+  return result;
+}
